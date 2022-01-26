3eec4381f6556416fe153a8f1f8760572938272b
Vincent <vincentbusiness55@gmail.com>
2022-01-26 10:50:17 +0800

    some fixes

--- start.py
+++ start.py
@@ -1,15 +1,18 @@
-import dotenv
-import os
 import logging
+import os
+
+import dotenv
 from rpd import BotApp
 
-logging.basicConfig(level=logging.DEBUG)  # i just do this since i develop the lib and love to see errors ofc!
+logging.basicConfig(level=logging.DEBUG)  # i just do this since i develop the lib and love to see errors and fuck ups!
 dotenv.load_dotenv()
 
 bot = BotApp(token=os.getenv("token"), intents=32767)
 
-async def add():
-    await bot.change_presence("h", 0)
+"""async def add():
+    await bot.change_presence("h", 0)"""
 
-bot.state.loop.create_task(add())
+if bot.state._ready.is_set() is True:
+    print("Bot is ready!")
+
 bot.run()
